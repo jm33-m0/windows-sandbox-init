dcad57a3d596546b85538c817b159382a03779ba
jm33-m0 <jm33@jm33.me>
2025-01-31 17:08:56 +1100

    installer functions

--- init.ps1
+++ init.ps1
@@ -11,26 +11,41 @@ function log_message {
         [string] $message
     )
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    "$timestamp - $message" | Out-File -FilePath $logFile -Append
+    $logEntry = "$timestamp - $message"
+    $logEntry | Out-File -FilePath $logFile -Append
+    Write-Output $logEntry
+}
+
+function check_error {
+    param (
+        [string] $errorMessage
+    )
+    if ($LASTEXITCODE -ne 0) {
+        log_message $errorMessage
+        throw $errorMessage
+    }
 }
 
 log_message "Script started."
 
-# Set Explorer to show file extension names and all hidden files including system files
-$showAllFiles = @(
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Hidden",
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ShowSuperHidden",
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\HideFileExt"
-)
-$values = 1, 1, 0
-$propertyNames = "Hidden", "ShowSuperHidden", "HideFileExt"
-for ($i = 0; $i -lt $showAllFiles.Length; $i++) {
-    if (-not (Test-Path $showAllFiles[$i])) {
-        New-Item -Path $showAllFiles[$i] -Force | Out-Null
-        log_message "Created registry path: $showAllFiles[$i]"
+function config_explorer() {
+    # Set Explorer to show file extension names and all hidden files including system files
+    $showAllFiles = @(
+        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Hidden",
+        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ShowSuperHidden",
+        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\HideFileExt"
+    )
+    $values = 1, 1, 0
+    $propertyNames = "Hidden", "ShowSuperHidden", "HideFileExt"
+    for ($i = 0; $i -lt $showAllFiles.Length; $i++) {
+        if (-not (Test-Path $showAllFiles[$i])) {
+            New-Item -Path $showAllFiles[$i] -Force | Out-Null
+            log_message "Created registry path: $showAllFiles[$i]"
+        }
+        Set-ItemProperty -Path $showAllFiles[$i] -Name $propertyNames[$i] -Value $values[$i]
+        check_error "Failed to set $propertyNames[$i] to $values[$i] at $showAllFiles[$i]"
+        log_message "Set $propertyNames[$i] to $values[$i] at $showAllFiles[$i]"
     }
-    Set-ItemProperty -Path $showAllFiles[$i] -Name $propertyNames[$i] -Value $values[$i]
-    log_message "Set $propertyNames[$i] to $values[$i] at $showAllFiles[$i]"
 }
 
 function create_shortcut {
@@ -45,27 +60,47 @@ function create_shortcut {
     log_message "Created shortcut for $targetPath on desktop."
 }
 
+function install_msi {
+    param (
+        [string] $msiPath
+    )
+    log_message "Installing MSI: $msiPath"
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/quiet", "/norestart", "/i", $msiPath -Wait
+    check_error "Failed to install $msiPath"
+    log_message "Installed $msiPath"
+}
+
+function install_nsis {
+    param (
+        [string] $nsisPath
+    )
+    log_message "Installing $nsisPath"
+    Start-Process -FilePath $nsisPath -ArgumentList "/S" -Wait
+    check_error "Failed to install $nsisPath"
+    log_message "Installed $nsisPath"
+}
+
 function process_files {
     param (
         [string] $path,
         [string] $filter,
-        [string] $processCommand,
-        [string[]] $arguments
+        [scriptblock] $callback_function
     )
     Get-ChildItem -Path $path -Filter $filter | ForEach-Object {
-        Start-Process -FilePath $processCommand -ArgumentList $arguments + $_.FullName -Wait
-        log_message "Processed $filter file $($_.FullName) with $processCommand"
+        $filePath = $_.FullName
+        & $callback_function $filePath
     }
 }
 
 # Install all MSI files in the source directory
-process_files -path $Src -filter "*.msi" -processCommand "msiexec.exe" -arguments @("/i", "/quiet", "/norestart")
+process_files -path $Src -filter "*.msi" -callback_function { param($filePath) install_msi $filePath }
 
 # Run all EXE files in the source directory with /S argument
-process_files -path $Src -filter "*.exe" -processCommand $_.FullName -arguments @("/S")
+process_files -path $Src -filter "*.exe" -callback_function { param($filePath) install_nsis $filePath }
 
 # Config Notepad++ with config.xml
 Copy-Item -Path .\config.xml -Destination "$env:APPDATA\Notepad++\config.xml" -Force
+check_error "Failed to copy config.xml to Notepad++ directory"
 log_message "Copied config.xml to Notepad++ directory."
 # Make shortcut for Notepad++ on desktop
 create_shortcut -shortcutPath "$env:HOME\Desktop\Notepad++.lnk" -targetPath "C:\Program Files\Notepad++\notepad++.exe"
@@ -75,6 +110,7 @@ $sevenZipPath = "C:\Program Files\7-Zip\7z.exe"
 Get-ChildItem -Path $Src -Filter *.zip | ForEach-Object {
     $destination = Join-Path "$Home\Desktop" ($_.BaseName)
     Start-Process -FilePath $sevenZipPath -ArgumentList "x", $_.FullName, "-o$destination", "-y" -Wait
+    check_error "Failed to unzip $($_.FullName) to $destination using 7-Zip"
     log_message "Unzipped $($_.FullName) to $destination using 7-Zip"
 }
 # Make shortcut for 7-Zip on desktop
